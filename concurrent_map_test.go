77ed6198ca4eb91d47654445dff4d6b5b57d3f3f
xiao7737 <773708486@qq.com>
2020-06-06 19:24:32 +0800

    fix concurrent_map_test.go

--- concurrent_map_test.go
+++ concurrent_map_test.go
@@ -1,6 +1,9 @@
 package concurrentMap
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+)
 
 func TestCreateConcurrentMap(t *testing.T) {
 	cm := CreateConcurrentMap(32)
@@ -14,9 +17,64 @@ func TestCreateConcurrentMap(t *testing.T) {
 
 func TestConcurrentMap_Set(t *testing.T) {
 	cm := CreateConcurrentMap(32)
-	cm.Set(ConvertStr("first, "), "live")
-	cm.Set(ConvertStr("then, "), "die")
+	cm.Set(ConvertStr("first, "), "hello")
+	cm.Set(ConvertStr("then, "), "bye")
 	if cm.Count() != 2 {
 		t.Error("map should contain two elements")
+	}
+}
+
+func TestConcurrentMap_Get(t *testing.T) {
+	cm := CreateConcurrentMap(32)
+	cm.Set(ConvertStr("hello"), "go")
+	cm.Set(ConvertInt64(333), 666)
+	res1, ok := cm.Get(ConvertStr("hello"))
+	if res1 != "go" || !ok {
+		t.Error("string key get err")
+	}
+	res2, ok := cm.Get(ConvertInt64(333))
+	if res2 != 666 || !ok {
+		t.Error("int64 key get err")
+	}
+}
+
+func TestConcurrentMap_Del(t *testing.T) {
+	cm := CreateConcurrentMap(32)
+	cm.Set(ConvertStr("hello"), "go")
+	cm.Set(ConvertInt64(333), 666)
+	cm.Del(ConvertInt64(333))
+	if cm.Count() != 1 {
+		t.Error("map should only have one key")
+	}
+	res, ok := cm.Get(ConvertInt64(666))
+	if res != nil || ok {
+		t.Error("map should not contain this key")
+	}
+	// Delete a key not in the map
+	cm.Del(ConvertStr("usb"))
+}
+
+func TestConcurrentMap_Count(t *testing.T) {
+	cm := CreateConcurrentMap(32)
+	for i := 0; i < 512; i++ {
+		cm.Set(ConvertStr("str"+strconv.Itoa(i)), i)
+		cm.Set(ConvertInt64(int64(i)), i)
+	}
+	if cm.Count() != 1024 {
+		t.Error("the number of elements in the map should be 1024")
+	}
+}
+
+func TestConcurrentMap_Exists(t *testing.T) {
+	cm := CreateConcurrentMap(32)
+	cm.Set(ConvertStr("hello"), "go")
+	cm.Set(ConvertInt64(333), 666)
+
+	if !cm.Exists(ConvertStr("hello")) {
+		t.Error("hello should be in the map")
+	}
+
+	if cm.Exists(ConvertInt64(444)) {
+		t.Error("444 should no is exists in the map")
 	}
 }
